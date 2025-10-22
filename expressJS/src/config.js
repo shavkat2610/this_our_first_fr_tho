e44f78fc53976dc64224a7eb0e532235ac62adbc
shavkat95 <shavkat95@icloud.com>
2025-10-22 23:18:27 +0200

    changing ports for db

--- expressJS/src/config.js
+++ expressJS/src/config.js
@@ -6,7 +6,7 @@ const readFileSync = filename => fs.readFileSync(filename).toString("utf8");
 module.exports = {
   database: {
     host: process.env.DATABASE_HOST || "localhost",
-    port: process.env.DATABASE_PORT || 3306,
+    port: process.env.DATABASE_PORT || 3309,
     database: process.env.DATABASE_DB || "example",
     user: process.env.DATABASE_USER || "root",
     password: process.env.DATABASE_PASSWORD
